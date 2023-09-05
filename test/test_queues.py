8022548b3e686796184fd1de9e5ab33663a7479c
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-09-05 14:29:22 -0400

    Update and rename test_queue.py to test_queues.py

--- test/test_queues.py
+++ test/test_queues.py
@@ -1,8 +1,10 @@
 import numpy as np
 import sys
 sys.path.append("/home/nick/Queue/src")
-from queue import production
+from queues import production
 
+
+np.random.seed(0)
 times = np.random.exponential(30, size=1000).astype(int).tolist()
 stations = 5
 
