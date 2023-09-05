7615c3c7191bc3d99adcad4122423d4fed685ad3
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-09-05 12:40:43 -0400

    Update test_queue.py

--- test/test_queue.py
+++ test/test_queue.py
@@ -1,1 +1,10 @@
+import numpy as np
+import sys
+sys.path.append("/home/nick/Queue/src")
+from queue import production
 
+times = np.random.exponential(30, size=1000).astype(int).tolist()
+stations = 5
+
+line = production(times, stations)
+print(f"Total Time: {line.total_time}")
